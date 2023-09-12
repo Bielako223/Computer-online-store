64b7b5ebda2a0d333805902f85f695adc8f0fee7
Bielako223 <mateusz-bielaszewski@o2.pl>
2023-09-12 13:52:12 +0200

    not working cart

---
+++ onlineStore/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreLibrary.DataAccess;
+using StoreLibrary.Migrations;
+using StoreLibrary.Models;
+
+namespace OnlineStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ShoppingCartModel _ShoppingCart;
+        private readonly IitemData _itemData;
+
+        public CartController(ShoppingCartModel shoppingCartModel, IitemData itemData)
+        {
+            _ShoppingCart= shoppingCartModel;   
+            _itemData= itemData;
+        }
+        public ViewResult Index()
+        {
+            var items = _ShoppingCart.GetShoppingCartItems();
+            _ShoppingCart.ShoppingCartItems = items;
+
+            var sCVM = new ShoppingCartViewModel
+            {
+                ShoppingCart = _ShoppingCart,
+                ShoppingCartTotal = _ShoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(sCVM);
+        }
+
+        public async Task<RedirectToActionResult> AddToCart(int itemId)
+        {
+           var item = await _itemData.GetItemById(itemId);
+            if (item !=null)
+            {
+                _ShoppingCart.AddToCart(item,1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<RedirectToActionResult> RemoveFromCart(int itemId)
+        {
+            var item = await _itemData.GetItemById(itemId);
+            if (item != null)
+            {
+                _ShoppingCart.RemoveFromCart(item);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ onlineStore/Models/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreLibrary.Models
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCartModel ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
