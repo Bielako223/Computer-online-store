df1849950b0109d62f47687741c814a7ad05649e
Bielako223 <mateusz-bielaszewski@o2.pl>
2023-09-15 23:28:07 +0200

    working cart, beta menu

---
+++ onlineStore/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreLibrary.Models;
+
+namespace OnlineStore.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ShoppingCartModel _ShoppingCart;
+        public MenuController(ShoppingCartModel shoppingCartModel)
+        {
+            _ShoppingCart= shoppingCartModel;
+        }
+        public PartialViewResult Menu()
+        { 
+            var amount = _ShoppingCart.GetShoppingCartTotal();
+            return PartialView(amount);
+        }
+    }
+}
