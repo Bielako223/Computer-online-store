b1330eb9e2482ea1d7749fba050ae0b95700f324
Mateusz Bielaszewski <mateusz-bielaszewski@o2.pl>
2023-09-16 14:04:14 +0200

    shoppingCart with counter

---
+++ onlineStore/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreLibrary.Models;
+
+namespace OnlineStore.ViewComponents
+{
+    public class Menu : ViewComponent
+    {
+        private readonly ShoppingCartModel _ShoppingCart;
+        public Menu(ShoppingCartModel shoppingCartModel)
+        {
+            _ShoppingCart = shoppingCartModel;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var amount = _ShoppingCart.GetShoppingCartItems().Count();
+
+            return View(amount);
+        }
+    }
+}
